d5251811e3363c6776909c3f9c030665d8d47be1
Shail <shailpatel67@gmail.com>
2019-10-21 20:58:55 -0400

    error handling and multi line comments

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <signal.h>
 #include <time.h>
 #include <stdarg.h>
+#include <ctype.h>
 #include "main.h"
 
 /* 
@@ -30,6 +31,9 @@ void sigcapt(int sig){
    RUN = 0; 
 }
 
+//given n strings, glue them together
+//if ADDSPACE is true a space will be added
+//between each string
 int ADDSPACE = 1;
 char * concat(int n, ...){
     if (n <= 0)
@@ -68,51 +72,133 @@ char * concat(int n, ...){
     return ret;
 }
 
+//is a command symmetric, ie complete
+//this can usually be done by checking 
+//if theres a semi-colon at the end
+
+//for things like loops and structs check if the parens and 
+//curly brackets line up
+int isSymmetric(char * str){
+    //start at the back and see if we hit a ';'
+    char * c = str + strlen(str) - 1;
+    while(*c){
+        if (*c == ';' && LCURLY == RCURLY && LPAREN == RPAREN)
+            return 1;
+        
+        if (isalnum(*c))
+            break;
+        
+        c--;
+    } 
+    
+    //update the count of parens and brackets
+    c = str;
+    while(*c){
+        switch(*c){
+            case '{': RCURLY++; break;
+            case '}': LCURLY++; break;
+            case '(': LPAREN++; break;
+            case ')': RPAREN++; break;
+        } 
+        c++;
+    }
+    
+    int sym = RCURLY == LCURLY && LPAREN == RPAREN;
+    
+    if (!sym)
+        return 0; 
+    
+    //if they are equal check if Parens are greater than zero
+    //if Curlys then we can finish
+    
+    if (LCURLY > 0)
+        return 1;
+    
+    if (LPAREN > 0)
+        return 0;
+}
+
+void readErr(void){
+    system("cat .out");
+}
+
+//if the .out file is empty no stderr, otherwise
+//rollback
+int didErr(void){
+    //check file, we can't use O_APPEND since
+    //we need to file offset to check the size 
+    int fd = open(".out", O_RDONLY); 
+    int len = lseek(fd, 0, SEEK_END);
+    close(fd);
+
+    return len > 0; 
+}
+
 void repl(int fd, char * fname){
     char buf[MAXBUF]; 
     char cwd[MAXBUF];
+    char tgt[10];
     
     if (!getcwd(cwd,MAXBUF-1)){
         fprintf(stderr, "Failed to get current working directory\n");
         return;
     } 
 
-    int len,r,w;
+    int len,r,w = 0;
     while(RUN){
         //READ
         printf(">");
         if(! fgets(buf, MAXBUF, stdin))
             break; 
         
+        if(!RUN)
+            break;
+
         len = strlen(buf); 
         
         lseek(fd, 0, SEEK_END);
-        w = write(fd, buf, len);
-        if (w != len){
-            fprintf(stderr, "Failed to write to file skipping\n");
+        w += write(fd, buf, len);
+
+        if (!isSymmetric(buf)){
+            //multiline code, continue for now 
             continue;
-        }  
-    
+        }
 
         //EVAL
-        write(fd, "}", strlen("}")); 
-        char tgt[10];
+        w += write(fd, "}", strlen("}")); 
         strcpy(tgt, fname);
         tgt[strlen(tgt)-2] = '\0';
+
         //compile
         ADDSPACE = 1;
         char * cmd = concat(6, "cc", fname, "-o", tgt, "2>", ".out");
         system(cmd);
         free(cmd);
+        
+        int len = lseek(fd, 0, SEEK_END);
+
+        //did anything go wrong?
+        if (didErr()){
+            //undo w bytes and skip execution
+            ftruncate(fd, len-w);
+            readErr();
+            w = 0;
+            continue;
+        }         
+        
+        w = 0; 
         //execute
         ADDSPACE = 0;
         char * exc = concat(2, "./", tgt);
         system(exc);
         free(exc);
-        int len = lseek(fd, 0, SEEK_END);
+
         ftruncate(fd, len-1);
         
     } 
+
+    //cleanup the compiled target
+    int ret = unlink(tgt);
 }
 
 int main(int argc, char ** argv){
--- main.h
+++ main.h
@@ -10,5 +10,10 @@
 #define MAXBUF 1024
 
 int SAVEFD = 0;
+//symmetric check
+int LPAREN = 0;
+int RPAREN = 0;
+int LCURLY = 0;
+int RCURLY = 0;
 
 #endif
