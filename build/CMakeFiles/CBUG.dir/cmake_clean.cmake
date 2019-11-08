b7752666fa583ff4d7dd8cbbde83c773405f0df1
Shail <shailpatel67@gmail.com>
2019-11-07 23:02:12 -0500

    add cmake

---
+++ build/CMakeFiles/CBUG.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CBUG.dir/main.c.o"
+  "CBUG.pdb"
+  "CBUG"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CBUG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
